bca1e05c4884f8b46798d698671a80e0bcc85b13
Chris Herwig <chris@mapbox.com>
2013-01-04 18:24:35 -0500

    add new demos to index page

--- site.js
+++ site.js
@@ -1,6 +1,79 @@
+var maps = ["gfdrr.dhaka-3d-building-use", "gfdrr.dhaka-building-survey-images", "gfdrr.dhaka-building-plans", "gfdrr.dhaka-building-footprints,gfdrr.dhaka-building-labels", "gfdrr.dhaka-gfdrr-roads"]
+var divs = ["bld-use", "bld-img", "bld-plan", "bld-labels", "gfdrr-roads"]
+mapbox.load('gfdrr.map-n7mlul8g,gfdrr.dhaka-gfdrr-roads,' + maps[0], function(o) {
+	var map = mapbox.map(divs[0]);
+	map.addLayer(o.layer).zoom(17).center({
+		lat: 23.7481,
+		lon: 90.3965
+	}).setPanLimits([{
+		lat: 23.6845,
+		lon: 90.3315
+	}, {
+		lat: 23.8909,
+		lon: 90.464
+	}]);
+	map.setZoomRange(11, 19).interaction.auto();
+});
 
 
-mapbox.auto('bld-use', 'http://a.tiles.mapbox.com/v3/gfdrr.dhaka-3d-building-use.jsonp');
-mapbox.auto('bld-img', 'http://a.tiles.mapbox.com/v3/gfdrr.dhaka-building-survey-images.jsonp');
-mapbox.auto('bld-plan', 'http://a.tiles.mapbox.com/v3/gfdrr.dhaka-building-plans.jsonp');
+mapbox.load('gfdrr.map-n7mlul8g,gfdrr.dhaka-gfdrr-roads,' + maps[1], function(o) {
+	var map = mapbox.map(divs[1]);
+	map.addLayer(o.layer).zoom(17).center({
+		lat: 23.74348,
+		lon: 90.41046
+	}).setPanLimits([{
+		lat: 23.6845,
+		lon: 90.3315
+	}, {
+		lat: 23.8909,
+		lon: 90.464
+	}]);
+	map.setZoomRange(11, 19).interaction.auto();		
+});
 
+
+mapbox.load('gfdrr.map-fpb8n04o,' + maps[2], function(o) {
+	var map = mapbox.map(divs[2]);
+	map.addLayer(o.layer).zoom(18).center({
+		lat: 23.72458,
+		lon: 90.37680
+	}).setPanLimits([{
+		lat: 23.6845,
+		lon: 90.3315
+	}, {
+		lat: 23.8909,
+		lon: 90.464
+	}]);
+	map.setZoomRange(11, 19).interaction.auto();
+});
+
+
+mapbox.load('gfdrr.map-fpb8n04o', function(o) {
+	var map = mapbox.map(divs[3]);
+	map.addLayer(o.layer).zoom(16).center({
+		lat: 23.7279,
+		lon: 90.4061
+	}).setPanLimits([{
+		lat: 23.6845,
+		lon: 90.3315
+	}, {
+		lat: 23.8909,
+		lon: 90.464
+	}]);
+	map.setZoomRange(11, 19).interaction.auto();
+	
+});
+
+var m = mapbox.map('osm');
+m.addLayer(new MM.TemplatedLayer('http://b.tile.openstreetmap.org/{Z}/{X}/{Y}.png')).zoom(16).center({
+	lat: 23.7279,
+	lon: 90.4061
+}).setPanLimits([{
+	lat: 23.6845,
+	lon: 90.3315
+}, {
+	lat: 23.8909,
+	lon: 90.464
+}]).setZoomRange(11, 18).ui.attribution.add().content('Map tiles by <a href="http://stamen.com">Stamen Design</a>, under' + ' <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' + ' Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.');
+m.ui.zoomer.add();
+m.ui.zoombox.add();
