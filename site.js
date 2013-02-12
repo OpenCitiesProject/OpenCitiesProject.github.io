82d8138dc8c753920b650b36df199345f9ce8420
tristen <tristen@tristen.ca>
2013-02-12 17:36:03 -0500

    First large chunk of thiw work in place

--- site.js
+++ site.js
@@ -1,98 +1,39 @@
-var maps = [
-    'gfdrr.dhaka-3d-building-use',
-    'gfdrr.dhaka-building-survey-images',
-    'gfdrr.dhaka-building-plans',
-    'gfdrr.dhaka-building-footprints,gfdrr.dhaka-building-labels',
-    'gfdrr.dhaka-gfdrr-roads'
-    ]
+;(function(context) {
 
-var divs = [
-    'bld-use',
-    'bld-img',
-    'bld-plan',
-    'bld-labels',
-    'gfdrr-roads'
-]
+    var gfdrr = {}
 
-mapbox.load('gfdrr.map-nkb1ordy,' + maps[0], function(o) {
-	var map = mapbox.map(divs[0]);
-	map.addLayer(o.layer).zoom(17).center({
-		lat: 23.7481,
-		lon: 90.3965
-	}).setPanLimits([{
-		lat: 23.6845,
-		lon: 90.3315
-	}, {
-		lat: 23.8909,
-		lon: 90.464
-	}]);
-	map.ui.legend.add();		
-	map.setZoomRange(11, 18).interaction.auto();
-});
+    gfdrr.slideShow = function(context) {
+        var slideIndex = 1,
+            $slide = $('[data-index]', context),
+            slides = $slide.length;
 
+        $('a.slide-control').click(function() {
+            $slide.removeClass('active');
 
-mapbox.load('gfdrr.map-n7mlul8g,gfdrr.dhaka-gfdrr-roads,' + maps[1], function(o) {
-	var map = mapbox.map(divs[1]);
-	map.addLayer(o.layer).zoom(17).center({
-		lat: 23.74348,
-		lon: 90.41046
-	}).setPanLimits([{
-		lat: 23.6845,
-		lon: 90.3315
-	}, {
-		lat: 23.8909,
-		lon: 90.464
-	}]);
-	map.setZoomRange(11, 18).interaction.auto();
-	map.ui.legend.add();		
-	
-});
+            if (slideIndex === slides) {
+                slideIndex = 1;
+            } else if ((slideIndex - 1) === 1) {
+                slideIndex = slides; 
+            } else {
+                // Adjust the counter
+                if ($(this).hasClass('next')) {
+                   slideIndex = (slideIndex + 1); 
+                } else {
 
+                    console.log(slideIndex);
+                    if (slideIndex <= 1) {
+                        slideIndex = slides; 
+                    } else {
+                        slideIndex = (slideIndex - 1); 
+                    }
+                }
+            }
 
-mapbox.load('gfdrr.map-fpb8n04o,' + maps[2], function(o) {
-	var map = mapbox.map(divs[2]);
-	map.addLayer(o.layer).zoom(18).center({
-		lat: 23.72458,
-		lon: 90.37680
-	}).setPanLimits([{
-		lat: 23.6845,
-		lon: 90.3315
-	}, {
-		lat: 23.8909,
-		lon: 90.464
-	}]);
-	map.setZoomRange(11, 18).interaction.auto();
-	map.ui.legend.add();		
-	
-});
+           $('[data-index="slide-' + slideIndex + '"]', context).addClass('active');
+            return false;
+        });
+    };
 
+    window.gfdrr = gfdrr;
+})(window);
 
-mapbox.load('gfdrr.map-fpb8n04o', function(o) {
-	var map = mapbox.map(divs[3]);
-	map.addLayer(o.layer).zoom(16).center({
-		lat: 23.7279,
-		lon: 90.4061
-	}).setPanLimits([{
-		lat: 23.6845,
-		lon: 90.3315
-	}, {
-		lat: 23.8909,
-		lon: 90.464
-	}]);
-	map.setZoomRange(11, 18).interaction.auto();
-	
-});
-
-var m = mapbox.map('osm');
-m.addLayer(new MM.TemplatedLayer('http://b.tile.openstreetmap.org/{Z}/{X}/{Y}.png')).zoom(16).center({
-	lat: 23.7279,
-	lon: 90.4061
-}).setPanLimits([{
-	lat: 23.6845,
-	lon: 90.3315
-}, {
-	lat: 23.8909,
-	lon: 90.464
-}]).setZoomRange(11, 18).ui.attribution.add().content('Map tiles by <a href="http://stamen.com">Stamen Design</a>, under' + ' <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' + ' Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.');
-m.ui.zoomer.add();
-m.ui.zoombox.add();
